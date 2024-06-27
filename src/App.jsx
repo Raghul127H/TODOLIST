ab33ce5cbbf373a73d7081707f2608409705208f
Raghul127H <vraghul127@gmail.com>
2024-06-27 13:35:33 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import Todolist from './component/Todolist'
+
+
+
+export default function App() {
+  return (
+    <div>
+
+     
+      <Todolist/>
+     
+     
+
+
+
+    </div>
+  )
+}
